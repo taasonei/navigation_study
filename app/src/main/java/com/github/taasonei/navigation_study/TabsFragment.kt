ea5df2b62274fc9b3300aa4b186b4d15b920ca39
Anna <taasonei@gmail.com>
2024-01-08 21:22:10 +0300

    tabs navigation

--- app/src/main/java/com/github/taasonei/navigation_study/TabsFragment.kt
+++ app/src/main/java/com/github/taasonei/navigation_study/TabsFragment.kt
@@ -1,7 +1,34 @@
 package com.github.taasonei.navigation_study
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.github.taasonei.navigation_study.databinding.FragmentTabsBinding
 
-class TabsFragment : Fragment(R.layout.fragment_tabs) {
+class TabsFragment : Fragment() {
 
+    private var _binding: FragmentTabsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentTabsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navHostFragment = childFragmentManager.findFragmentById(R.id.tabsContainer) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.tabsBottomBar.setupWithNavController(navController)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
